77b15dd77ceb0bd708dff99e526406193ed6fd6b
Shaheer-Imam <42369778+Shaheer-Imam@users.noreply.github.com>
2019-09-06 22:19:18 +0500

    One step at a time

--- scripts/script.py
+++ scripts/script.py
@@ -97,3 +97,53 @@ def get_error_msg(error,lang):
                 return m.group(1)
 
         return None
+
+def read(pipe,func):
+    for line in iter(pipe.readline,b''):
+        for fun in func:
+            fun(line.decode("utf-8"))
+    pipe.close()
+
+def write(get):
+    for line in iter(get,None):
+        print(line)
+
+# Main
+
+def execute(command):
+    process = Popen(
+        command,
+        cwd=None,
+        shell=False,
+        close_fds=True,
+        stdout=PIPE,
+        stderr=PIPE,
+        bufsize=1
+    )
+    output=[]
+    errors=[]
+    pipe_queue=Queue()
+
+    stdout_thread=Thread(target=read,args=(process.stdout, [pipe_queue.put, output.append]))
+    stderr_thread=Thread(target=read,args=(process.stderr, [pipe_queue.put, errors.append]))
+
+    writer_thread=Thread(target=write,args=(pipe_queue.get,))
+
+    for thread in(stdout_thread, stderr_thread, writer_thread):
+        thread.daemon=True
+        thread.start()
+
+    process.wait()
+
+    for thread in (stdout_thread,stderr_thread):
+        thread.join()
+
+    pipe_queue.put(None)
+
+    output=' '.join(output)
+    errors=' '.join(errors)
+
+    if "java"!=command[0] and not os.path.isfile(command[1]):
+        return (None,None)
+    else:
+        return (output,errors)
